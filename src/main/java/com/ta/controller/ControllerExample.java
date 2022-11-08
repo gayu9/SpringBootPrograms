5cc051d03b8646b358ab80d7006a46d49fbdbfe3
gayu9 <gayathrispandi@gmail.com>
2022-11-08 15:59:30 +0530

    success

--- src/main/java/com/ta/controller/ControllerExample.java
+++ src/main/java/com/ta/controller/ControllerExample.java
@@ -14,68 +14,68 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ta.Service.TestControllerImpl;
+//import com.ta.Service.TestControllerImpl;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/gayu/")
 
-public class TestController {
+public class ControllerExample {
 
 	String calculation;
-	@Autowired TestControllerImpl obj;
+//	@Autowired TestControllerImpl obj;
     @Value("${fpath}")
     private String filepath ;
    
 
-	@RequestMapping(value = "testAPI", method = RequestMethod.GET)
-	
-	public ResponseEntity<?> testApi(@RequestParam Map<String,String> requestParams)
-			throws Exception {
-		int ans=0;
-		String Status="Success";
-		try
-		{
-			Logger log = LogManager.getLogger("GETBatch");
-			log.debug("testAPI started: " + requestParams);
-			log.info("testAPI started: " + requestParams);
-
-		System.out.println(requestParams);
-		
-		String operation = requestParams.get("operation");
-		int n1 = Integer.parseInt(requestParams.get("number1"));
-		System.out.println("Number1"+n1);
-		int n2 = Integer.parseInt(requestParams.get("number2"));
-		System.out.println( "Number2"+n2);
-
-		switch (operation) {
-		case "add":
-			ans = obj.add(n1, n2);
-			break;
-		case "sub":
-			ans = obj.sub(n1, n2);
-			break;
-		case "multiply":
-			ans = obj.multiplication(n1, n2);
-			break;
-		case "division":
-
-			ans = obj.division(n1, n2);
-			break;
-
-		}
-		}catch(Exception e)
-		{
-			Status="Failiure";
-			System.out.println(e);
-		}
-		requestParams.put("Status", Status);
-		requestParams.put("Answer",ans+"");
-		
-		
-
-		return new ResponseEntity<>(requestParams, HttpStatus.OK);
-		
-	}
+//	@RequestMapping(value = "testAPI", method = RequestMethod.GET)
+//	
+//	public ResponseEntity<?> testApi(@RequestParam Map<String,String> requestParams)
+//			throws Exception {
+//		int ans=0;
+//		String Status="Success";
+//		try
+//		{
+//			Logger log = LogManager.getLogger("GETBatch");
+//			log.debug("testAPI started: " + requestParams);
+//			log.info("testAPI started: " + requestParams);
+//
+//		System.out.println(requestParams);
+//		
+//		String operation = requestParams.get("operation");
+//		int n1 = Integer.parseInt(requestParams.get("number1"));
+//		System.out.println("Number1"+n1);
+//		int n2 = Integer.parseInt(requestParams.get("number2"));
+//		System.out.println( "Number2"+n2);
+//
+//		switch (operation) {
+//		case "add":
+//			ans = obj.add(n1, n2);
+//			break;
+//		case "sub":
+//			ans = obj.sub(n1, n2);
+//			break;
+//		case "multiply":
+//			ans = obj.multiplication(n1, n2);
+//			break;
+//		case "division":
+//
+//			ans = obj.division(n1, n2);
+//			break;
+//
+//		}
+//		}catch(Exception e)
+//		{
+//			Status="Failiure";
+//			System.out.println(e);
+//		}
+//		requestParams.put("Status", Status);
+//		requestParams.put("Answer",ans+"");
+//		
+//		
+//
+//		return new ResponseEntity<>(requestParams, HttpStatus.OK);
+//		
+//	}
 	
 @RequestMapping(value = "loadProperties", method = RequestMethod.GET)
 	
